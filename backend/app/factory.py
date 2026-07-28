aa2fd79353c2f7c15a11c15744150d2f22a659fb
LMH-L-7092\ralazar <ralazar@lastmilehealth.org>
2026-07-28 14:51:40 +0300

    local-changes

--- backend/app/factory.py
+++ backend/app/factory.py
@@ -76,7 +76,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=allowed_origins,
-        allow_credentials=True,
+        allow_credentials=False,  # Must be False when allow_origins=["*"]
         allow_methods=["*"],
         allow_headers=["*"],
     )
